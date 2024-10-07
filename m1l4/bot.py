025387701f229da4580633dfc314317935917e44
Rodrigo768 <rodrigo.diaz@gmail.com>
2024-10-07 20:50:24 +0200

    primera mensaje

---
+++ m1l4/bot.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+import random
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix=':/', description=description, intents=intents)
+client = discord.Client(intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.reply('Hello!', mention_author=True)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('token')
+
+bot.run('MTI4OTYzMjg1NTAzMDQ5NzM3MQ.GM6e4A.Y_EJYCNt6XbtuJuwZ2aLgGubtqdQ5DMhO-T7Wc')
